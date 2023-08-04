d07e3f67b85704c0c7a239c926666a21893b91c7
timer4ik <timergalievadel@yandex.ru>
2023-08-04 21:16:30 +0300

    +add

--- database/migrations/2023_08_04_172230_create_posts_table.php
+++ database/migrations/2023_08_04_172230_create_posts_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->text('content');
-            $table->integer('count_like')->default(0);
-            $table->integer('count_comment')->default(0);
-            $table->integer('count_view')->default(0);
+            $table->integer('likes')->default(0);
+            $table->integer('comments')->default(0);
+            $table->integer('views')->default(0);
             $table->foreignId('theme_id')->nullable()->constrained();
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
