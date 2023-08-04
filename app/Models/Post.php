ab0a08226bba3172a60f43737c350262b664431c
timer4ik <timergalievadel@yandex.ru>
2023-08-04 20:22:59 +0300

    !fix post migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,28 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     use HasFactory;
-
-    private $fillable = [
-        'content',
-        'count_like',
-        'count_comment',
-        'count_view',
-        'user_id',
-        'theme_id'
-    ];
-
-    public function user(): BelongsTo
-    {
-        return $this->belongsTo(User::class);
-    }
-
-    public function theme(): BelongsTo
-    {
-        return $this->belongsTo(Theme::class);
-    }
 }
